f96c149fe910b1344a9a74ac7a26d1a1970befe5
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-12-08 00:53:48 +0200

    Add schema list, login and logout views

--- schemas/views.py
+++ schemas/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.views import generic
 
-# Create your views here.
+from schemas.forms import LoginForm
+from schemas.models import Schema
+
+
+class SchemaListView(LoginRequiredMixin, generic.ListView):
+    model = Schema
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.select_related("user").filter(user__id=self.request.user.id)
+
+
+class NewLoginView(LoginView):
+    form_class = LoginForm
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")

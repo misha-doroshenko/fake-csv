3d7ab3353e2f45d8cceb40426c1105d77be0a169
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-12-15 17:13:15 +0200

    Update delete schema url

--- schemas/urls.py
+++ schemas/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", SchemaListView.as_view(), name="schema-list"),
     path("schemas/create", SchemaCreateView.as_view(), name="schema-create"),
     path("schemas/update/<int:pk>/", SchemaUpdateView.as_view(), name="schema-update"),
-    path("schemas/<int:pk>/delete/", SchemaDeleteView.as_view(), name="schema-delete"),
+    path("schemas/delete/<int:pk>/", SchemaDeleteView.as_view(), name="schema-delete"),
     path("schemas/file_csv/<int:pk>/", schema_files, name="schema-files"),
 ]
